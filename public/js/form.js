aa722821680079cc7173759579ba0a47b48dfba6
leandrosilvarmos <leandro.silva.rmos@gmail.com>
2024-05-09 18:15:12 -0300

    Formulario ajustado com a mensagem de agradecimento, e projeto revisado, realizado o deploy

---
+++ public/js/form.js
@@ -0,0 +1,40 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Impede o envio padrão do formulário
+
+    // Coleta os dados do formulário
+    var formData = new FormData(this);
+
+    // Envia os dados para o Formspree usando Fetch API
+    fetch('https://formspree.io/f/moqgbval', {
+        method: 'POST',
+        body: formData,
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+    .then(function(response) {
+        // Se o envio for bem-sucedido, exibe uma mensagem de agradecimento por alguns segundos
+        if (response.ok) {
+            alert('Obrigado pelo seu feedback! Sua mensagem foi enviada com sucesso.');
+            // Limpa os campos do formulário
+            document.getElementById('nome').value = '';
+            document.getElementById('identidade').value = '';
+            document.getElementById('feedback').value = '';
+            document.getElementById('star1').checked = false;
+            document.getElementById('star2').checked = false;
+            document.getElementById('star3').checked = false;
+            document.getElementById('star4').checked = false;
+            document.getElementById('star5').checked = false;
+            // Redireciona para a página especificada após alguns segundos
+            setTimeout(function() {
+                window.location.href = 'https://leandrosilvarmos.github.io/portifolio_oficial/#feedbacks';
+            }, 3000); // 3000 milissegundos = 3 segundos
+        } else {
+            alert('Desculpe, algo deu errado. Por favor, tente novamente mais tarde.');
+        }
+    })
+    .catch(function(error) {
+        console.error('Ocorreu um erro:', error);
+        alert('Desculpe, algo deu errado. Por favor, tente novamente mais tarde.');
+    });
+});
